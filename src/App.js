7476b436c3f95124e5ffd82288f7aa18366bdb83
jenniavalos <115288260+jenniavalos@users.noreply.github.com>
2022-11-22 09:40:11 -0600

    Ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {ShowCards} from './components/ShowCards';
+import {CreateCard} from './components/CreateCard';
+import {EditCard} from './components/EditCard';
+
+
 function App() {
   return (
     <div className="App">
@@ -17,9 +23,22 @@ function App() {
         >
           Learn React
         </a>
+        <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<ShowCards/>}/>
+        <Route path='/create' element={<CreateCard/>} />
+        <Route path='/edit/:id' element={<EditCard/>} />
+       </Routes>
+       </BrowserRouter>
       </header>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
