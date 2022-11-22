6ca55f7dc551819a8db91f7109efbd51f4c04fec
JorgeMarroquin91 <jorgemarro91@hotmail.com>
2022-11-22 10:55:40 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import Menu from './components/Menu';
+import Mascota from './components/Mascota';
+import CreateMascota from './components/CreateMascota';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Menu/>
+    <div className='container'>        
+      <Routes>
+        <Route path='/' />
+        <Route path='/mascotas' element={<Mascota/>}/>
+        <Route path='/createmascota' element={<CreateMascota/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Mascota.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Mascota extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            mascotas: []
+        }
+    }
+
+    componentDidMount () {
+        this.fetchMascota();
+    }
+
+    fetchMascota = async () => {
+        let respuesta = await axios.get('http://127.0.0.1:8000/api/pet');
+        // eslint-disable-next-line react/no-direct-mutation-state
+        this.setState({mascotas: respuesta.data})
+    }
+
+    render () {
+        return ( <div>
+        <h2>Mascotas</h2>
+        <Link className="btn btn-success" to="/createmascota">Insertar Mascota</Link>
+        <br/>
+        <br/>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>IdMascota</th>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                    <th>Color</th>
+                    <th>Peso</th>
+                    <th>Sexo</th>
+                    <th>Activo</th>
+                    <th>Especie</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.mascotas.map(mascota=>{
+                    return (<tr>
+                        <td>{mascota.idMascota}</td>
+                        <td>{mascota.nombre}</td>
+                        <td>{mascota.edad}</td>
+                        <td>{mascota.color}</td>
+                        <td>{mascota.peso}</td>
+                        <td>{mascota.sexo}</td>
+                        <td>{mascota.activo}</td>
+                        <td>{mascota.idEspecie}</td>
+                        <td><button className="btn btn-default">Editar</button>
+                        <button className="btn btn-danger">Eliminar</button></td>
+                    </tr>)
+                })}
+            </tbody>
+        </table>
+        </div>
+        )
+    }
+
+}
