cda4f00acf7b0c21488d0c9636bc9677712f3ed9
JorgeMarroquin91 <jorgemarro91@hotmail.com>
2022-11-23 13:21:39 -0600

    dejando vacio app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {ShowCards} from './components/ShowCards';
-import {CreateCard} from './components/CreateCard';
-import {EditCard} from './components/EditCard';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <BrowserRouter>
-       <Routes>
-        <Route path='/' element={<ShowCards/>}/>
-        <Route path='/create' element={<CreateCard/>} />
-        <Route path='/edit/:id' element={<EditCard/>} />
-       </Routes>
-       </BrowserRouter>
-      </header>
-    </div>
+    <></>
   );
 }
 export default App;
